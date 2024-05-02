96b6a55ee2377f4fcde82ddc56cb7368ab33d044
Abdirehman <abdilsultan2021@gmail.com>
2024-05-03 02:53:36 +0300

    deploy

--- netflix-clone-2024/src/Components/Rows/Row/Row.jsx
+++ netflix-clone-2024/src/Components/Rows/Row/Row.jsx
@@ -3,10 +3,10 @@ import "./row.css";
 import axios from "../../../utilities/axios";
 import movieTrailer from "movie-trailer";
 import YouTube from "react-youtube";
-import ClipLoader from "react-spinners/ClipLoader";
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
-  const [movies, setMovie] = useState([]);
+  const [movies, setMovie] = useState(null);
   const [trailerUrl, setTrailerUrl] = useState("");
 
   const base_url = "https://image.tmdb.org/t/p/original";
@@ -30,10 +30,9 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
     } else {
       movieTrailer(movie?.title || movie?.name || movie?.original_name).then(
         (url) => {
-          console.log(url);
+          
           const urlParams = new URLSearchParams(new URL(url).search);
-          console.log(urlParams);
-          console.log(urlParams.get("v"));
+          
           setTrailerUrl(urlParams.get("v"));
         }
       );
@@ -45,10 +44,36 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
     width: "100%",
     playerVars: {
       autoplay: 1,
+      controls: "true",
     },
   };
 
- 
+  return (
+    <div className="row">
+      <h1>{title}</h1>
+      <div className="row__posters">
+        {movies ? (
+          movies?.map((movie, index) => (
+            <img
+              onClick={() => handleClick(movie)}
+              key={index}
+              src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            />
+          ))
+        ) : (
+          // Loading Animation
+          <PropagateLoader color="#36d7b7" />
+        )}
+      </div>
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </div>
+  );
 };
 
 export default Row;
